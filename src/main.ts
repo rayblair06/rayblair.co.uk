d7bfa0ca488cb06e4ac8832562ec89653b2ad61f
Ray Blair <rayblair06@hotmail.com>
2024-03-19 19:23:14 +0000

    Fixed styles

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // src/main.ts
 import { ViteSSG } from "vite-ssg/single-page";
+import "./style.css";
 import App from "./App.vue";
 
 // `export const createApp` is required instead of the original `createApp(App).mount('#app')`
