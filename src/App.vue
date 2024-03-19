1b8f37316d8454da26186250d59bfa73cf468d69
Ray Blair <rayblair06@hotmail.com>
2024-03-19 20:17:48 +0000

    Updated design

--- src/App.vue
+++ src/App.vue
@@ -46,15 +46,16 @@ const socialMedias: Array<SocialMedia> = [
 <template>
   <div class="flex items-center justify-center h-full">
     <div class="text-center">
-      <h1 class="uppercase tracking-wider text-4xl font-light mb-2">
+      <h1 class="uppercase tracking-wider text-5xl font-light mb-2">
         Ray Blair
       </h1>
-      <h2 class="uppercase tracking-wider text-2xl font-bold">
+
+      <h2 class="uppercase tracking-wider text-3xl font-bold">
         Software Engineer
       </h2>
 
       <img
-        class="mt-4 mb-8 w-[150px] h-[150px] max-w-[95%] rounded-[50%] border-[1px] border-solid border-black mx-auto"
+        class="mt-4 mb-8 w-[200px] h-[200px] max-w-[95%] rounded-[50%] border-[1px] border-solid border-black mx-auto"
         sizes="(max-width: 800px) 100vw, 800px"
         srcset="
           /images/1675778361600_l6miom_c_scale_w_200.webp 200w,
@@ -66,23 +67,21 @@ const socialMedias: Array<SocialMedia> = [
         alt="Ray Blair"
       />
 
-      <div id="social-icons" class="px-8">
+      <div class="px-12">
         <template v-for="socialMedia in socialMedias" :key="socialMedia.text">
           <a
             :href="socialMedia.url"
             target="_blank"
             :aria-label="`Ray Blair's ${socialMedia.text}`"
-            class="flex items-center justify-between w-full h-[25px] bg-slate-50 rounded-lg text-[#494d55] mb-3 pl-2 font-light tracking-wider text-sm uppercase py-4"
+            class="flex items-center justify-between w-full h-10 bg-slate-50 rounded-lg text-[#494d55] mb-4 pl-2 font-light tracking-widest uppercase py-4"
           >
             {{ socialMedia.text }}
 
             <span class="fa-stack fa-sm">
-              <font-awesome-layers class="fas fa-circle fa-stack-2x fa-inverse">
-                <FontAwesomeIcon
-                  class="fa-stack-1x text-[1.2rem] text-[#494d55]"
-                  :icon="socialMedia.icon"
-                />
-              </font-awesome-layers>
+              <font-awesome-icon
+                class="fa-stack-1x text-xl text-[#494d55]"
+                :icon="socialMedia.icon"
+              ></font-awesome-icon>
             </span>
           </a>
         </template>
@@ -90,50 +89,3 @@ const socialMedias: Array<SocialMedia> = [
     </div>
   </div>
 </template>
-
-<style lang="scss">
-// a {
-//   display: block;
-//   font-weight: 750;
-//   color: #fff;
-//   font-size: 1.2em;
-//   letter-spacing: 1px;
-//   text-transform: uppercase;
-//   text-decoration: none;
-// }
-
-// a:hover {
-//   display: block;
-//   color: #ededed;
-//   font-size: 1.2em;
-//   letter-spacing: 1px;
-//   font-weight: 750;
-//   text-transform: uppercase;
-//   text-decoration: none;
-// }
-
-// #social-icons {
-//   display: block;
-//   margin-top: 0;
-// }
-
-// #social-icons a {
-//   display: inline-block;
-//   opacity: 1;
-//   transition: all 1s;
-// }
-
-// #social-icons a:hover {
-//   display: inline-block;
-//   opacity: 0.4;
-// }
-
-// #social-icons img {
-//   margin: 5px;
-//   color: #fff;
-//   width: 25px;
-//   height: 25px;
-//   border: none;
-//   border-radius: 0;
-// }
-</style>
