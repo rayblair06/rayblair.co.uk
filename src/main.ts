cbaf34361dbec263b2645e26440573868febee2d
Ray Blair <rayblair06@hotmail.com>
2024-03-19 16:56:41 +0000

    SSG

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// src/main.ts
+import { ViteSSG } from "vite-ssg/single-page";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// `export const createApp` is required instead of the original `createApp(App).mount('#app')`
+export const createApp = ViteSSG(App);
