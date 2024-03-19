c11222739dca1cfbee20a9f94710746e5dffb747
Ray Blair <rayblair06@hotmail.com>
2024-03-19 19:58:36 +0000

    Installed critters and define ssg options

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Docs: https://github.com/antfu/vite-ssg | https://github.com/antfu/vite-ssg/blob/main/src/types.ts#L6
+  ssgOptions: {
+    crittersOptions: {
+      // Preload strategy - https://github.com/GoogleChromeLabs/critters#usage
+      preload: "media",
+      formatting: "minify",
+      script: "async",
+    },
+  },
 });
